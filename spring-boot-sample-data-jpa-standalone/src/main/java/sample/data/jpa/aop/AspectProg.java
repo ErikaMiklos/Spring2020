a0e6ff14462b3158befb301355291ae764a22778
ErikaMiklos <perkamma@gmail.com>
2022-09-24 22:52:30 +0200

    ajout aop logging

---
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/aop/AspectProg.java
@@ -0,0 +1,29 @@
+package sample.data.jpa.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class AspectProg {
+    /**
+     * Logging
+     */
+    private Logger logger = LoggerFactory.getLogger(AspectProg.class);
+
+    @Before(value = "execution(* sample.data.jpa.web.*.*(..))")
+    public void logDeclarationBefore(JoinPoint joinPoint){
+        logger.info("Executing {}", joinPoint);
+    }
+
+    @After(value = "execution(* sample.data.jpa.web.*.*(..))")
+    public void logDeclarationAfter(JoinPoint joinPoint){
+        logger.info("Complete execution of {}", joinPoint);
+    }
+
+}
