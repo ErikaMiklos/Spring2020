777a04adc6188ee9c8466b4c8e03d266d1421877
ErikaMiklos <perkamma@gmail.com>
2022-09-27 17:56:00 +0200

    correct annotation

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/aop/AspectProg.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/aop/AspectProg.java
@@ -6,14 +6,15 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class AspectProg {
     /**
      * Logging
      */
-    private Logger logger = LoggerFactory.getLogger(AspectProg.class);
+    private final Logger logger = LoggerFactory.getLogger(AspectProg.class);
 
     @Before(value = "execution(* sample.data.jpa.web.*.*(..))")
     public void logDeclarationBefore(JoinPoint joinPoint){
